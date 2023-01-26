c6ad8fa43babd786fb640321f8097270fea8d003
Vernyoon Chao <vernchao510@gmail.com>
2023-01-26 13:39:44 -0800

    used react-router-dom in order for certain pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,20 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  decrement,
-  increment,
-  getTheNumberFromHere,
-} from "./store/slices/testSlice";
+import { Routes, Route, redirect, Navigate } from "react-router-dom";
+
+import Home from "./components/home.js";
+import Example from "./components/testexample.js";
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-  const payload = {
-    gameName: "FF2",
-    genre: ["fantasy", "rpg"],
-    description: "Made in heaven plus another",
-    developer: "Square Enix",
-    publisher: "Square Enix",
-    price: 50.0,
-    earlyAccess: true,
-  };
+  const user = false;
 
   return (
-    <div>
-      <button
-        aria-label="Increment value"
-        onClick={async () => {
-          const response = await dispatch(getTheNumberFromHere(payload));
-          // console.log("clicked");
-          // console.log(response);
-        }}
-      >
-        Increment
-      </button>
-      <button
-        aria-label="Increment value"
-        onClick={() => dispatch(increment(payload))}
-      >
-        Increment
-      </button>
-      <span>{count}</span>
-      <button
-        aria-label="Decrement value"
-        onClick={() => dispatch(decrement())}
-      >
-        Decrement
-      </button>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/example" element={<Example></Example>}></Route>
+        <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
+      </Routes>
+    </>
   );
 }
 
